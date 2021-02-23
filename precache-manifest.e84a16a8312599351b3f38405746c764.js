7b98ee9514d624ee6c1b0276eb7ce802afb3c013
alejorminetti <alejorm@gmail.com>
2021-02-23 16:35:28 -0300

    Updates

---
+++ precache-manifest.e84a16a8312599351b3f38405746c764.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-react-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "aa77d9d1dd70747f8fbe",
+    "url": "/platzi-react-badges/static/css/main.76af6b72.chunk.css"
+  },
+  {
+    "revision": "34b414a620c13e8602b8",
+    "url": "/platzi-react-badges/static/js/runtime~main.34b414a6.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-react-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "4ddc11fdddb1d413e7fb",
+    "url": "/platzi-react-badges/static/js/2.4ddc11fd.chunk.js"
+  },
+  {
+    "revision": "aa77d9d1dd70747f8fbe",
+    "url": "/platzi-react-badges/static/js/main.aa77d9d1.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platzi-react-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-react-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platzi-react-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "4ddc11fdddb1d413e7fb",
+    "url": "/platzi-react-badges/static/css/2.ee637e98.chunk.css"
+  },
+  {
+    "revision": "c4ffc52006c388b0b7bf204092220f11",
+    "url": "/platzi-react-badges/index.html"
+  }
+];
